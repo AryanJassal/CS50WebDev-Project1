2d9346adbe116fad0dfe146b9c6c2663536b4e6f
AryanJassal <aryan.jassal@education.nsw.gov.au>
2020-08-09 19:48:31 +1000

    Links in index page working.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.listEntries()
+    })
+
+def displayEntry(request, title):
+    title = util.getEntry(title)
+    htmlConverted =  Markdown().convert(title)
+    return render(request, "encyclopedia/displayEntry.html", {
+        "mainBody": htmlConverted
     })
