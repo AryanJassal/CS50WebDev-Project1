855272cae19cfbd1148639c65076f9e4229196a5
AryanJassal <aryan.jassal@education.nsw.gov.au>
2020-08-12 18:43:59 +1000

    Tried to make random page working, still crashes

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("<str:title>", views.displayEntry, name = "displayEntry")
+    path("<str:title>", views.displayEntry, name = "displayEntry"),
+    path("random", views.randomPage, name = "randomEntry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+from random import choice
 
 from . import util
 
@@ -10,7 +11,16 @@ def index(request):
 
 def displayEntry(request, title):
     title = util.getEntry(title)
-    htmlConverted =  Markdown().convert(title)
+    htmlConverted = Markdown().convert(title)
     return render(request, "encyclopedia/displayEntry.html", {
         "mainBody": htmlConverted
     })
+
+def randomPage(request):
+    a = []
+    for entry in entries:
+        a.append(entry)
+
+    randomEntry = random.choice(a)
+    displayEntry(randomEntry)
+    
